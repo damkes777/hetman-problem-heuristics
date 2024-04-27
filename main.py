0e81c55ce1c782c63cca0e995270ec8d4611b84d
damkes777 <111892734+damkes777@users.noreply.github.com>
2024-04-27 10:45:31 +0200

    add time

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import heapq
+import time
 import hetman
 import heuristics_h1
 
 if __name__ == '__main__':
 	het = hetman.Hetman(4)
 	heuristics = heuristics_h1.HeuristicsH1(4)
-
 	open_queue = []
 	heapq.heappush(open_queue, (0, []))
 
+	time_start = time.time()
 	while open_queue:
 		priority, hetmans_array = heapq.heappop(open_queue)
 
@@ -22,7 +23,10 @@ if __name__ == '__main__':
 				for child in children:
 					hetman_heuristics = heuristics.calculate_heuristics(child)
 					heapq.heappush(open_queue, (hetman_heuristics, child))
+	time_end = time.time()
+	final_time = (time_end - time_start) * 1000
 
+	print(final_time)
 	print(het.solutions)
 	print(het.generated_counter)
 	print(het.visited_counter)
